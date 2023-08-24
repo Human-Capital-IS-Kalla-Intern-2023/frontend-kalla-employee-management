1e703344a461ea4be56ab29c762eb06516d16d5b
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-08-24 13:15:39 +0800

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      primary: '#0BA854',
+      white: '#fff',
+      black: '#2F2F41',
+      pureBlack: '#000',
+      link: '#3b82f6',
+    },
+    fontFamily: {
+      body: ['Poppins'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
