76f3d06575b2a6d4a934873382bce2e40dc5fc44
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-08-25 10:30:43 +0800

    update branch vercel

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,7 +2,9 @@ export default function Login() {
   return (
     <>
       <div className="justify-center flex items-center h-screen">
-        <h1 className="text-center font-bold text-3xl">WELCOME TO DASHBOARD</h1>
+        <h1 className="text-center font-bold text-3xl">
+          WELCOME TO DASHBOARD PAGE
+        </h1>
       </div>
     </>
   );
