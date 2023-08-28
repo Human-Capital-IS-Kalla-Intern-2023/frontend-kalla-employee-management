fcbc9e5f00000b1bed0ae704a51df0d48638aab7
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-08-28 22:05:48 +0800

    fix conflict

--- src/pages/NotAllowed.tsx
+++
@@ -1,5 +0,0 @@
-const NotAllowed = () => {
-  return <h1 className="">NotAllowed Page</h1>;
-};
-
-export default NotAllowed;
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,13 +1,6 @@
-
-const NotFound = () => {
-  return <h1 className="">NotFound Page</h1>;
-};
-
-export default NotFound;
-
 import ErrorCard404 from '../components/errorpage/ErrorCard404';
 
-export default function Login() {
+export default function NotFound() {
   return (
     <>
       <ErrorCard404 />
