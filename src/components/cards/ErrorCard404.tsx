09b3ce9aa32596728d44b14d9b2918944f551e89
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-09-04 23:58:13 +0800

    update error page card

--- src/components/cards/ErrorCard404.tsx
+++ src/components/cards/ErrorCard404.tsx
@@ -1,17 +1,23 @@
-import ErrorIcon from '../../assets/img/not-found.svg';
+import NotFoundImage from '../../assets/img/NotFoundImage.svg';
 import { NavLink } from 'react-router-dom';
 
 const ErrorCard404 = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen space-x-8 space-y-16 lg:flex-row lg:space-y-0 2xl:space-x-0">
-      <div className="flex flex-col items-center justify-center w-full ml-40 text-center lg:w-2/4 lg:px-2 xl:px-5">
-        <p className="text-6xl font-bold tracking-wider text-black text-gray-300 md:text-8xl lg:text-7xl">
-          404
-        </p>
-        <p className="mt-2 text-3xl font-bold tracking-wider text-black text-gray-300 md:text-5xl lg:text-4xl">
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      <div className="flex flex-col items-center space-y-8 text-center">
+        <div>
+          <img
+            className="w-full max-w-4xl"
+            src={NotFoundImage}
+            alt="404 Not Found"
+            width={300}
+            height={200}
+          />
+        </div>
+        <p className="mt-2 text-3xl font-bold tracking-wider text-black text-gray-300 md:text-lg lg:text-3xl">
           Page Not Found
         </p>
-        <p className="my-12 text-lg text-black text-gray-500 md:text-xl lg:text-2xl">
+        <p className="my-12 text-lg text-black text-gray-500 md:text-base lg:text-lg">
           Sorry, the page you are looking for could not be found!
         </p>
         <li
@@ -19,12 +25,9 @@ const ErrorCard404 = () => {
           title="Return Home"
         >
           <NavLink to={'/'}>
-            <span className="text-2xl text-white">Return Home</span>
+            <span className="text-xl text-white">Return Home</span>
           </NavLink>
         </li>
-      </div>
-      <div className="flex justify-center w-2/3 p-4 lg:h-full lg:items-center">
-        <img className="w-10/12 " src={ErrorIcon} />
       </div>
     </div>
   );
