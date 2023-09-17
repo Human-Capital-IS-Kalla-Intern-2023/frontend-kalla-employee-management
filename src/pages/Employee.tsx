87831dd27b1bce22c900f3fc507a13dd8a521a51
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-09-17 11:27:45 +0800

    Update Employee Page

--- src/pages/Employee.tsx
+++ src/pages/Employee.tsx
@@ -1,154 +1,186 @@
-import React from 'react';
+// Import Library & Package
+import React, { useEffect, useState } from 'react';
+
+// Import Component
 import TabelHeader from '../components/tabels/TabelHeader';
-// import TabelFooter from '../components/tabels/TabelFooter';
+import TabelFooter from '../components/tabels/TabelFooter';
 import TabelBody from '../components/tabels/TabelBody';
+import { SuccessAlert, ErrorAlert } from '../components/alerts/CustomAlert';
+import { ResetAlert } from '../helpers/ResetAlert';
 
-const colCells = [
-  { key: 'id', text: 'No' },
-  { key: 'fullName', text: 'Full Name' },
-  { key: 'nik', text: 'NIK' },
-  { key: 'email', text: 'Email' },
-  { key: 'jabatanUtama', text: 'Jabatan Utama' },
-  { key: 'jabatanLainnya', text: 'Jabatan Lainnya' },
-];
+// Import API
+import {
+  getEmployee,
+  getDetailEmployee,
+  addEmployee,
+  updateEmployee,
+  deleteEmployee,
+} from '../api/EmployeeAPI';
 
-const inputField = [
-  {
-    id: 'employee_name',
-    label: 'Nama Karyawan',
-    name: 'Nama Karyawan',
-    type: 'text',
-  },
-  {
-    id: 'nip',
-    label: 'NIP',
-    name: 'NIP',
-    type: 'text',
-  },
-  {
-    id: 'nickname',
-    label: 'Nick Name',
-    name: 'Nick Name',
-    type: 'text',
-  },
-  {
-    id: 'hiredate',
-    label: 'Tanggal Masuk',
-    name: 'Tanggal Masuk',
-    type: 'date',
-  },
-  {
-    id: 'companyemail',
-    label: 'Company Email',
-    name: 'Company Email',
-    type: 'email',
-  },
-  {
-    id: 'mainposition',
-    label: 'Posisi Utama',
-    name: 'Posisi Utama',
-    type: 'text',
-  },
-  {
-    id: 'secondaryposition',
-    label: 'Posisi Lainnya',
-    name: 'Posisi Lainnya',
-    type: 'text',
-  },
-];
-
-const data = [
-  {
-    id: 1,
-    fullName: 'John Doe',
-    nik: '1234567890123456',
-    email: 'john.doe@example.com',
-    jabatanUtama: 'Manager',
-    jabatanLainnya: 'Supervisor',
-  },
-  {
-    id: 2,
-    fullName: 'Jane Smith',
-    nik: '7890123456123456',
-    email: 'jane.smith@example.com',
-    jabatanUtama: 'Supervisor',
-    jabatanLainnya: 'Team Lead',
-  },
-  {
-    id: 3,
-    fullName: 'Michael Johnson',
-    nik: '4567890123456789',
-    email: 'michael.johnson@example.com',
-    jabatanUtama: 'Associate',
-    jabatanLainnya: 'Assistant',
-  },
-  {
-    id: 4,
-    fullName: 'Emily Williams',
-    nik: '2345678901234567',
-    email: 'emily.williams@example.com',
-    jabatanUtama: 'Associate',
-    jabatanLainnya: 'Intern',
-  },
-  {
-    id: 5,
-    fullName: 'Robert Brown',
-    nik: '6789012345678901',
-    email: 'robert.brown@example.com',
-    jabatanUtama: 'Manager',
-    jabatanLainnya: 'Supervisor',
-  },
-  {
-    id: 6,
-    fullName: 'John Doe',
-    nik: '1234567890123456',
-    email: 'john.doe@example.com',
-    jabatanUtama: 'Manager',
-    jabatanLainnya: 'Supervisor',
-  },
-  {
-    id: 7,
-    fullName: 'Jane Smith',
-    nik: '7890123456123456',
-    email: 'jane.smith@example.com',
-    jabatanUtama: 'Supervisor',
-    jabatanLainnya: 'Team Lead',
-  },
-  {
-    id: 8,
-    fullName: 'Michael Johnson',
-    nik: '4567890123456789',
-    email: 'michael.johnson@example.com',
-    jabatanUtama: 'Associate',
-    jabatanLainnya: 'Assistant',
-  },
-  {
-    id: 9,
-    fullName: 'Emily Williams',
-    nik: '2345678901234567',
-    email: 'emily.williams@example.com',
-    jabatanUtama: 'Associate',
-    jabatanLainnya: 'Intern',
-  },
-  {
-    id: 10,
-    fullName: 'Robert Brown',
-    nik: '6789012345678901',
-    email: 'robert.brown@example.com',
-    jabatanUtama: 'Manager',
-    jabatanLainnya: 'Supervisor',
-  },
-];
-const filterOptions = [{ id: 'Location', label: 'Location' }];
-
-const handleAddEmployee = async () => {};
-const handleEditEmployee = async () => {};
-const handleDeleteEmployee = async () => {};
+import {
+  colCells,
+  filterOptions,
+  inputField,
+} from '../assets/data/EmployeeData';
 
 const Employee: React.FC = () => {
+  // Alert State
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [successTitle, setSuccessTitle] = useState<string | null>(null);
+  const [errorTitle, setErrorTitle] = useState<string | null>(null);
+
+  // Employee State
+  const [employee, setEmployee] = useState<string[]>([]);
+  const [detailedData, setDetailedData] = useState<string | null>(null);
+
+  // Pagination
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemsPerPage = 10;
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const totalDataCount = employee.length;
+  const totalPages = Math.ceil(totalDataCount / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage + 1;
+  const endIndex =
+    currentPage === totalPages ? totalDataCount : startIndex + itemsPerPage - 1;
+  const curretEmployeeData = employee.slice(startIndex - 1, endIndex);
+
+  // GET all employee data
+  const featchEmployee = async () => {
+    try {
+      const reponseData = await getEmployee();
+      setEmployee(reponseData.data);
+    } catch (error: any) {
+      console.error('Error featch all employee:', error);
+      setErrorTitle(`Error featch all employee`);
+
+      const errorMessages = Object.values(error.response.data.errors).flat();
+      setErrorMessage(errorMessages.join('\n'));
+    }
+  };
+
+  // GET detail employee data by id
+  const featchDetailEmployee = async (id: number) => {
+    try {
+      const responseData = await getDetailEmployee(id);
+      setDetailedData(responseData.data);
+    } catch (error: any) {
+      console.error('Error featch detail employee:', error);
+      setErrorTitle(`Error featch detail employee`);
+
+      const errorMessages = Object.values(error.response.data.errors).flat();
+      setErrorMessage(errorMessages.join('\n'));
+
+      ResetAlert(
+        setSuccessTitle,
+        setSuccessMessage,
+        setErrorTitle,
+        setErrorMessage
+      );
+    }
+  };
+
+  // POST new employee data
+  const handleAddEmployee = async (formData: string) => {
+    try {
+      const responseData = await addEmployee(formData);
+      setSuccessTitle(`${responseData.status}`);
+      setSuccessMessage(`${responseData.message}`);
+
+      featchEmployee();
+
+      ResetAlert(
+        setSuccessTitle,
+        setSuccessMessage,
+        setErrorTitle,
+        setErrorMessage
+      );
+    } catch (error: any) {
+      console.error('Error adding employee:', error);
+      setErrorTitle(`Error adding employee`);
+
+      const errorMessages = Object.values(error.response.data.errors).flat();
+      setErrorMessage(errorMessages.join('\n'));
+
+      ResetAlert(
+        setSuccessTitle,
+        setSuccessMessage,
+        setErrorTitle,
+        setErrorMessage
+      );
+    }
+  };
+
+  // PUT employee data
+  const handleEditEmployee = async (formData: string, id: number) => {
+    try {
+      const responseData = await updateEmployee(id, formData);
+
+      setSuccessTitle(`${responseData.status}`);
+      setSuccessMessage(`${responseData.message}`);
+      featchEmployee();
+
+      ResetAlert(
+        setSuccessTitle,
+        setSuccessMessage,
+        setErrorTitle,
+        setErrorMessage
+      );
+    } catch (error: any) {
+      console.error('Error editing employee:', error);
+      setErrorTitle(`Error editing employee`);
+      const errorMessages = Object.values(error.response.data.errors).flat();
+      setErrorMessage(errorMessages.join('\n'));
+    }
+  };
+
+  // DELETE employee data
+  const handleDeleteEmployee = async (id: number) => {
+    try {
+      const responseData = await deleteEmployee(id);
+      setSuccessTitle(`${responseData.status}`);
+      setSuccessMessage(`${responseData.message}`);
+      featchEmployee();
+
+      ResetAlert(
+        setSuccessTitle,
+        setSuccessMessage,
+        setErrorTitle,
+        setErrorMessage
+      );
+    } catch (error: any) {
+      console.error('Error deleting employee:', error);
+      setErrorTitle(`Error deleting employee`);
+
+      const errorMessages = Object.values(error.response.data.errors).flat();
+      setErrorMessage(errorMessages.join('\n'));
+
+      ResetAlert(
+        setSuccessTitle,
+        setSuccessMessage,
+        setErrorTitle,
+        setErrorMessage
+      );
+    }
+  };
+
+  useEffect(() => {
+    featchEmployee();
+  }, []);
+
   return (
     <>
-      <h1 className='px-4'>Employee Management Page</h1>
+      <h1 className="px-4">Employee Page</h1>
+      {successMessage && successTitle && (
+        <SuccessAlert title={successTitle} text={successMessage} />
+      )}
+      {errorMessage && errorTitle && (
+        <ErrorAlert title={errorTitle} text={errorMessage} />
+      )}
       <TabelHeader
         addButtonText="Add Employee"
         title="Add Employee"
@@ -159,13 +191,22 @@ const Employee: React.FC = () => {
       <TabelBody
         title="Edit Employee"
         colCells={colCells}
-        data={data}
+        data={curretEmployeeData}
         inputFields={inputField}
         onSubmit={handleEditEmployee}
         onDelete={handleDeleteEmployee}
+        detailedData={detailedData}
+        fetchDetailedData={featchDetailEmployee}
       />
-
-      {/* <TabelFooter /> */}
+      <TabelFooter
+        currentPage={currentPage}
+        totalPages={totalPages}
+        itemsPerPage={itemsPerPage}
+        totalDataCount={totalDataCount}
+        onPreviousPage={() => setCurrentPage(currentPage - 1)}
+        onNextPage={() => setCurrentPage(currentPage + 1)}
+        onPageChange={handlePageChange}
+      />
     </>
   );
 };
