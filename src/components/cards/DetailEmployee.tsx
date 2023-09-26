d2fef6d3c910243313aee00fce456f8b98296d60
Shouta002 <reynaldilight16@gmail.com>
2023-09-26 12:34:58 +0800

    Modified Text Size and Font Weight DetailEmployee.tsx

--- src/components/cards/DetailEmployee.tsx
+++ src/components/cards/DetailEmployee.tsx
@@ -62,15 +62,15 @@ const DetailEmployee = ({ employeeData } : any) => {
                             {/* Kolom 1 */}
                             <td className="px-4 py-2 text-left align-top ">
                               <div>
-                                <h2 className="text-lg font-medium">Company Name</h2>
+                                <h2 className="text-base font-semibold">Company Name</h2>
                                 <p className="text-base border-b pb-1">{employeeData.company}</p>
                               </div>
                               <div>
-                                <h2 className="text-lg font-medium pt-3">Directorate</h2>
+                                <h2 className="text-base font-semibold pt-3">Directorate</h2>
                                 <p className="text-base border-b pb-1">{employeeData.directorate}</p>
                               </div>
                               <div>
-                                <h2 className="text-lg font-medium pt-3 ">Division</h2>
+                                <h2 className="text-base font-semibold pt-3 ">Division</h2>
                                 <p className="text-base border-b pb-1">{employeeData.division}</p>
                               </div>
                             </td>
@@ -78,11 +78,11 @@ const DetailEmployee = ({ employeeData } : any) => {
                             {/* Kolom 2 */}
                             <td className="px-4 py-2 text-left align-top">
                               <div>
-                                <h2 className="text-lg font-medium">Section</h2>
+                                <h2 className="text-base font-semibold">Section</h2>
                                 <p className="text-base border-b pb-1">{employeeData.section}</p>
                               </div>
                               <div>
-                                <h2 className="text-lg font-medium pt-3">Main Position</h2>
+                                <h2 className="text-base font-semibold pt-3">Main Position</h2>
                                 <p className="text-base border-b pb-1">{employeeData.mainPosition}</p>
                               </div>
                             </td>
@@ -102,7 +102,7 @@ const DetailEmployee = ({ employeeData } : any) => {
                         <h2 className="text-base font-bold">Secondary Position</h2>
                       </th>
                       <th className="px-4 py-2 text-right border-b-2 w-1/2">
-                      </th>
+                          </th>
                     </tr>
                   </thead>
                   <tbody>
@@ -139,26 +139,28 @@ const DetailEmployee = ({ employeeData } : any) => {
                         <th className="px-4 py-2 text-left border-b-2 w-1/2">
                           <h2 className="text-base font-bold">Secondary Position Details</h2>
                         </th>
-                        <th className="px-4 py-2 text-right border-b-2 w-1/2"></th>
+                        <th className="px-4 py-2 text-right border-b-2 w-1/2">
+                          </th>
                       </tr>
                     </thead>
                     <tbody>
                       {employeeData.secondaryPosition
                         .filter((position: { position: string; }) => position.position === selectedSecondaryPosition)
                         .map((selectedPosition: { position: React.Key | null | undefined; company: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; directorate: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; division: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; section: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }) => (
+                          
                           <tr key={selectedPosition.position}>
                             {/* Kolom 1 */}
                             <td className="px-4 py-2 text-left align-top">
                               <div>
-                                <h2 className="text-lg font-medium">Company Name</h2>
+                                <h2 className="text-base font-semibold">Company Name</h2>
                                 <p className="text-base border-b pb-1">{selectedPosition.company}</p>
                               </div>
                               <div>
-                                <h2 className="text-lg font-medium pt-3">Directorate</h2>
+                                <h2 className="text-base font-semibold pt-3">Directorate</h2>
                                 <p className="text-base border-b pb-1">{selectedPosition.directorate}</p>
                               </div>
                               <div>
-                                <h2 className="text-lg font-medium pt-3">Division</h2>
+                                <h2 className="text-base font-semibold pt-3">Division</h2>
                                 <p className="text-base border-b pb-1">{selectedPosition.division}</p>
                               </div>
                             </td>
@@ -166,10 +168,10 @@ const DetailEmployee = ({ employeeData } : any) => {
                             {/* Kolom 2 */}
                             <td className="px-4 py-2 text-left align-top">
                               <div>
-                                <h2 className="text-lg font-medium">Section</h2>
+                                <h2 className="text-base font-semibold">Section</h2>
                                 <p className="text-base border-b pb-1">{selectedPosition.section}</p>
                               </div>
-                              {/* Add more details here */}
+                              
                             </td>
                           </tr>
                         ))}
@@ -181,16 +183,18 @@ const DetailEmployee = ({ employeeData } : any) => {
 
 
                 <div className='px-5'>
-                  <div className="bg-white rounded-lg pt-2 shadow-md my-4">
+                  <div className="bg-white rounded-lg pt-2 shadow-md my-4 cursor-pointer"
+                onClick={togglePrimaryAssignment} >
                     <table className="table-auto p-5 w-full">
                       <thead>
                         <tr>
                           <th className="px-4 py-2 text-left border-b-2 w-1/2">
-                            <h2 className="text-ml font-bold ">Allowance Information</h2>
+                            <h2 className="text-base font-bold ">Allowance Information</h2>
                           </th>
                           <th className="px-4 py-2 text-right border-b-2 w-1/2">
                             <button onClick={togglePrimaryAssignment}>
-                              {showPrimaryAssignment ? 'Hide' : 'Show'}
+                              {showPrimaryAssignment}
+                              <ArrowButtonIcon className="h-6 w-6 ml-1" />
                             </button>
                           </th>
                         </tr>
@@ -201,16 +205,16 @@ const DetailEmployee = ({ employeeData } : any) => {
                             {/* Kolom 1 */}
                             <td className="px-4 py-2 text-left align-top ">
                               <div>
-                                <h2 className="text-base ">Positional Allowance</h2>
+                                <h2 className="text-base font-semibold">Positional Allowance</h2>
                                 <p className="pl-7 text-sm border-b pb-1">Entitled</p>
                               </div>
                               <div>
-                                <h2 className="text-base pt-3">Communication Allowance</h2>
+                                <h2 className="text-base font-semibold pt-3">Communication Allowance</h2>
                                 <p className="pl-7 text-sm">Entitled</p>
                                 <p className="pl-7 text-sm border-b pb-1">Regulation</p>
                               </div>
                               <div>
-                                <h2 className="text-base pt-3 ">Transportation Allowance</h2>
+                                <h2 className="text-base font-semibold pt-3 ">Transportation Allowance</h2>
                                 <p className="pl-7 text-sm border-b pb-1">Entitled</p>
                               </div>
                             </td>
@@ -218,15 +222,15 @@ const DetailEmployee = ({ employeeData } : any) => {
                             {/* Kolom 2 */}
                             <td className="px-4 py-2 text-left align-top">
                               <div>
-                                <h2 className="text-base">Functional Allowance</h2>
+                                <h2 className="text-base font-semibold">Functional Allowance</h2>
                                 <p className="pl-7 text-sm border-b pb-1">Entitled</p>
                               </div>
                               <div>
-                                <h2 className="text-base pt-3">Meals Allowance</h2>
+                                <h2 className="text-base font-semibold pt-3">Meals Allowance</h2>
                                 <p className="pl-7 text-sm border-b pb-6">Entitled</p>
                               </div>
                               <div>
-                                <h2 className="text-base pt-3">Parking Allowance</h2>
+                                <h2 className="text-base font-semibold pt-3">Parking Allowance</h2>
                                 <p className="pl-7 text-sm border-b pb-1">Entitled</p>
                               </div>
                             </td>
