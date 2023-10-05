7e9d4e89140aa21c0ca1a3dd8835bc3dfdbc27f1
Shouta002 <reynaldilight16@gmail.com>
2023-10-05 13:06:44 +0800

    Use API at EligiblesEmployee and EditEligibles

--- src/pages/Employee/EditEligibles.tsx
+++ src/pages/Employee/EditEligibles.tsx
@@ -1,24 +1,45 @@
-import { useParams } from 'react-router-dom';
 import EditEligiblesCard from '../../components/cards/EditEligiblesCard';
+import { useParams } from 'react-router-dom';
+import { getDetailEmployee} from '../../api/EmployeeAPI';
+import { getGrade} from '../../api/GradeAPI';
+import { useEffect, useState } from 'react';
+import { SuccessAlert, ErrorAlert } from '../../components/alerts/CustomAlert';
 
 const EditEligibles = () => {
   const { employeeId } = useParams();
+  const [detailedData, setDetailedData] = useState<string | null>(null);
+  
 
-  const employeeData = {
-    name: 'John Doe',
-    employeeId: 'EMP12345',
-    company: 'ABC Corp',
-    directorate:'Technology Information',
-    division:'Website Developer',
-    jobGrade:"A",
-    mainPosition: 'Marketing Manager',
-    profileImageUrl: '/src/assets/img/ProfilePicture.jpg',
+  // Alert State
+  const [successMessage] = useState<string | null>(null);
+  const [errorMessage] = useState<string | null>(null);
+  const [successTitle] = useState<string | null>(null);
+  const [errorTitle] = useState<string | null>(null);
+
+  const featchDetailEmployee = async (id: any) => {
+    try {
+      const responseData = await getDetailEmployee(id);
+      setDetailedData(responseData.data);
+    } catch (error: any) {
+      console.error('Error featch detail employee:', error);
+    }
   };
+
+  useEffect(() => {
+    featchDetailEmployee(employeeId);
+  }, [employeeId]);
 
   return (
     <>
-      <h1 className="px-4">Edit Eligibles Employee</h1>
-      <EditEligiblesCard employeeData={employeeData} />
+      {successMessage && successTitle && (
+        <SuccessAlert title={successTitle} text={successMessage} />
+      )}
+      {errorMessage && errorTitle && (
+        <ErrorAlert title={errorTitle} text={errorMessage} />
+      )}
+      <EditEligiblesCard
+        employeeData={detailedData}
+      />
     </>
   );
 };
