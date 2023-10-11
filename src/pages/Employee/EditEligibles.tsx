5135be2a161c531064ad27e5068434db078f0997
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-10-11 23:42:23 +0800

    add loading when featching eligibles data

--- src/pages/Employee/EditEligibles.tsx
+++ src/pages/Employee/EditEligibles.tsx
@@ -1,14 +1,15 @@
 import EditEligiblesCard from '../../components/cards/EditEligiblesCard';
 import { useParams } from 'react-router-dom';
-import { getDetailSalaryEmployee } from '../../api/EmployeeAPI';
+import { getEditSalaryEmployee } from '../../api/EmployeeAPI';
 import { useEffect, useState } from 'react';
 import { SuccessAlert, ErrorAlert } from '../../components/alerts/CustomAlert';
+import ReactLoading from 'react-loading';
 
 const EditEligibles = () => {
   const { employeeId } = useParams();
   const { positionId } = useParams();
 
-  const [detailedData, setDetailedData] = useState<string | null>(null);
+  const [detailedData, setDetailedData] = useState<any | null>(null);
 
   // Alert State
   const [successMessage] = useState<string | null>(null);
@@ -18,13 +19,10 @@ const EditEligibles = () => {
 
   const featchDetailEmployee = async (employeeId: any, positionId: any) => {
     try {
-      const responseData = await getDetailSalaryEmployee(
-        employeeId,
-        positionId
-      );
+      const responseData = await getEditSalaryEmployee(employeeId, positionId);
       setDetailedData(responseData.data);
     } catch (error: any) {
-      console.error('Error featch detail employee:', error);
+      console.error('Error featch detail edit employee:', error);
     }
   };
 
@@ -32,6 +30,13 @@ const EditEligibles = () => {
     featchDetailEmployee(employeeId, positionId);
   }, [employeeId, positionId]);
 
+  if (!detailedData) {
+    return (
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+        <ReactLoading type="spin" color="green" height={50} width={50} />
+      </div>
+    );
+  }
   return (
     <>
       {successMessage && successTitle && (
