929a93965155db8b2b1673c3cdea51ad8ae27083
Shouta002 <reynaldilight16@gmail.com>
2023-10-13 13:55:31 +0800

    Responsive (2)

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,14 +1,13 @@
 import DashboardCard from '../../components/cards/DashboardCard';
 
 const Dashboard = () => {
-  
   return (
-  <>
-  <h1 className="px-4">Dashboard Page</h1>
-  
+    <>
+      <h1 className="px-4">Dashboard Page</h1>
+
       <DashboardCard />
     </>
-)
+  );
 };
 
 export default Dashboard;
