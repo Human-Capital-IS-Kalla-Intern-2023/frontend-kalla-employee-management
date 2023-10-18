eb461f8190fdeb86ea43480f0b1362c71d246152
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-10-18 17:03:17 +0800

    Tes 2

---
+++ src/pages/auth/Unauthorized.tsx
@@ -0,0 +1,11 @@
+import ErrorCard401 from '../../components/cards/error/ErrorCard401';
+
+const Unauthorized = () => {
+  return (
+    <>
+      <ErrorCard401 />
+    </>
+  );
+};
+
+export default Unauthorized;
