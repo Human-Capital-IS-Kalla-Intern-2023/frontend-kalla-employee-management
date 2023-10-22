693cae356b13e096831c14c4cc71b204f4915347
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-10-22 21:38:10 +0800

    Add Compesation People Page V1

---
+++ src/pages/salary/compensation/CompensationPeopleEdit.tsx
@@ -0,0 +1,6 @@
+import CompensationPeopleEditCard from '../../../components/cards/compensation/CompensationPeopleEditCard';
+const CompensationPeopleEdit = () => {
+  return <CompensationPeopleEditCard />;
+};
+
+export default CompensationPeopleEdit;
