e6e7a1b0f42f8f15c0d9845ee7ec20224b9102ed
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-10-27 16:57:40 +0800

    Add Delete Text  For Every Delete Modal

--- src/components/modals/DeleteModal.tsx
+++ src/components/modals/DeleteModal.tsx
@@ -1,4 +1,4 @@
-const DeleteModal = ({ isOpen, onClose, onDelete }: any) => {
+const DeleteModal = ({ isOpen, onClose, onDelete, deleteData }: any) => {
   const handleDelete = () => {
     onDelete();
     onClose();
@@ -17,24 +17,25 @@ const DeleteModal = ({ isOpen, onClose, onDelete }: any) => {
       onClick={handleOverlayClick}
     >
       <div className="w-1/3 p-6 bg-white rounded shadow-lg">
-        <h2 className="mb-4 text-xl font-semibold text-center text-red-600">
+        <h2 className="mb-4 text-[22px] font-semibold text-center text-red-600">
           Delete Confirmation
         </h2>
         <div className="text-lg text-center text-black">
-          Are you sure you want to delete this?
+          Are you sure you want to delete <br />
+          <span className="font-semibold">{deleteData}</span> ?
         </div>
         <div className="flex justify-end mt-8">
           <button
             aria-label="Cancel"
             onClick={onClose}
-            className="w-full px-4 py-2 mr-4 text-white bg-green-800 rounded overlay hover:bg-green-600"
+            className="w-full px-4 py-2 mr-4  text-base text-black rounded bg-[#EAEAEC] overlay hover:bg-slate-300  duration-200"
           >
             Cancel
           </button>
           <button
             aria-label="Delete"
             onClick={handleDelete}
-            className="w-full px-4 py-2 text-white bg-red-800 rounded hover:bg-red-600"
+            className="w-full px-4 py-2 text-base text-white bg-red-800 rounded hover:bg-red-600"
           >
             Delete
           </button>
---
+++ src/helpers/DeleteText.tsx
@@ -0,0 +1,29 @@
+export const DeleteText = (
+  customCell: Record<string, any>,
+  locationPathname: string
+): string => {
+  if (locationPathname.includes('/employee')) {
+    return customCell.fullname;
+  } else if (locationPathname.includes('/position')) {
+    return customCell.position_name;
+  } else if (locationPathname.includes('/grade')) {
+    return customCell.grade_name;
+  } else if (locationPathname.includes('/business')) {
+    return customCell.company_name;
+  } else if (locationPathname.includes('/directorate')) {
+    return customCell.directorat_name;
+  } else if (locationPathname.includes('/division')) {
+    return customCell.division_name;
+  } else if (locationPathname.includes('/section')) {
+    return customCell.section_name;
+  } else if (locationPathname.includes('/location')) {
+    return customCell.location_name;
+  } else if (locationPathname.includes('/salary/regulation')) {
+    return customCell.component_name;
+  } else if (locationPathname.includes('/salary/configures')) {
+    return customCell.salary_name;
+  } else if (locationPathname.includes('/salary/compensation')) {
+    return customCell.compensation_name;
+  }
+  return '';
+};
